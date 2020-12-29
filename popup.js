c539ddce3dd450a9b722306c760f613ca22bc17f
tomy li <tomy.li@corp.to8to.com>
2020-12-29 11:39:54 +0800

    功能增加更多选项；代码提高代码复用

--- popup.js
+++ popup.js
@@ -3,25 +3,42 @@ var copyUrlObj = document.getElementById('copyUrl')
 var createUrlText = document.getElementById('createUrlText')
 var tip = document.getElementById('tips')
 var btn = document.getElementById('createUrl')
+var pageTypes = document.getElementsByName('pageType')
 window.onload = function () {
   btn && btn.addEventListener('click', function () {
-    if (fullurlObj.value == '') {
-      showError('请输入链接')
-      return
-    }
-    createUrlText.innerHTML = createUrl(fullurlObj.value)
-    copyUrl()
+    createAndCopy()
   })
 
   copyUrlObj && copyUrlObj.addEventListener('click', function () {
-    if (fullurlObj.value == '') {
-      showError('请输入链接')
-      return
-    }
-    copyUrl()
+    createAndCopy()
   })
-}
 
+  for (el of pageTypes) {
+    el.addEventListener("change", function() {
+      createAndCopy()
+    })
+  }
+}
+function createAndCopy() {
+  if (fullurlObj.value == '') {
+    showError('请输入链接')
+    return
+  }
+  var urlType = getSelectUrlType()
+  if (!urlType) {
+    return showError('请选择链接投放渠道类型')
+  }
+  createUrlText.innerHTML = createUrl(fullurlObj.value, urlType)
+  copyUrl()
+}
+function getSelectUrlType() {
+  for( i = 0; i < pageTypes.length; i++) {
+    if(pageTypes[i].checked) {
+      return   pageTypes[i].value;
+    }
+  }
+  return 0
+}
 function copy_clipper () {
   createUrlText.focus();
   createUrlText.select();
@@ -36,9 +53,20 @@ function copy_clipper () {
 function copyUrl() {
   copy_clipper()
 }
-function createUrl (url) {
+function createUrl (url, urlType) {
+  var miniUrl = ''
+  switch (urlType) {
+    case '1':
+      miniUrl = '/pages/view/view'
+      break;
+    case '2':
+      miniUrl = '/pages/index/index'
+      break;
+    default:
+      miniUrl = '/pages/view/view'
+  }
   // 小程序webview页面会自动补齐项目基础路径部分，因此这里截取掉那部分
-  return '/pages/view/view?url=' + encodeURIComponent(url.replace('https://xxxx.com/xxx', ''))
+  return miniUrl + '?url=' + encodeURIComponent(url.replace('https://x.xx.com/xx', ''))
 }
 function showError (message) {
   showTip(message, '错误')
